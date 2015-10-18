7540328af4373580f7ad39a79afc9de806e04c0f
My <My@192.168.1.70>
2015-10-18 22:28:18 +0300

    1

---
+++ Test/src/main/Start.java
@@ -0,0 +1,22 @@
+package main;
+
+import java.util.Vector;
+
+public class Start implements TestInterface {
+
+	public static void main(String... args) {
+		int i = 1;
+		int i2 = 2;
+		int i3 = 3;
+
+		Vector vector = new Vector();
+		vector.add(i);
+		vector.add(i2);
+		vector.add(i3);
+		System.out.println(vector);
+
+		System.out.println(vector);
+
+	}
+
+}
---
+++ Test/src/main/TestInterface.java
@@ -0,0 +1,9 @@
+package main;
+
+public interface TestInterface {
+
+	public String my1 = "Hello1";
+	public static String my2 = "Hello2";
+	public static final String my3 = "Hello3";
+
+}
